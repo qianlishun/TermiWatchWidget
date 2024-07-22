749850e21d5a17b507fd7965b65ceef64bb10ac9
mrq <511940597@qq.com>
2024-07-22 11:43:20 +0800

    Add default font, adjust the title layout

--- TermiWatchWidget Watch App/View/QCommonView.swift
+++ TermiWatchWidget Watch App/View/QCommonView.swift
@@ -44,7 +44,7 @@ struct WeatherRectangularView : View {
         VStack(alignment: .leading,spacing: 0) {
             HStack {
                 if((weather.dateText != nil) && weather.dateText!.count > 0){
-                    Text("[DATE]").font(.system(size: qFontSize+0.5))
+                    MyText("[DATE]",fontSize: qFontSize+0.5)
                     MyText(weather.dateText!).frame(maxWidth: .infinity, alignment: .leading).minimumScaleFactor(0.8)
                 }else{
                     MyText("user@\(terminalName):~ $ now").frame(maxWidth: .infinity, alignment: .leading)
@@ -52,13 +52,13 @@ struct WeatherRectangularView : View {
             }.frame(height: rowHeight)
             if(weather.alert.count>0){
                 HStack {
-                    MyText("[ALER.]")
+                    MyText("[ALER]",fontSize: qFontSize+0.5)
                     Image(systemName: "exclamationmark.triangle").frame(width: 16).imageScale(.small).foregroundStyle(colorAlert1).minimumScaleFactor(0.8)
                     Text(weather.alert).font(font).foregroundStyle(colorAlert1)
                 }.frame(height: rowHeight)
             }
             HStack {
-                MyText("[CuRR]")
+                MyText("[CURR]").kerning(-0.2)
                 WXImage(wxIcon: weather.current.symbol).foregroundStyle(color: colorCond)
                 Text(weather.current.condition).font(font).frame( maxWidth: .infinity,alignment: .leading).foregroundStyle(colorCond).minimumScaleFactor(0.8)
             }.frame(height: rowHeight)
@@ -74,14 +74,14 @@ struct WeatherRectangularView : View {
             }.frame(height: rowHeight)
             
             HStack {
-                MyText("[HUMI]")
+                MyText("[HUMI]",fontSize: qFontSize+0.5).kerning(-0.1)
                 Image(systemName: "humidity").frame(width: 15).imageScale(.small).foregroundStyle(colorHumi)
                 Text(weather.current.humidity).font(font).frame( maxWidth: .infinity,alignment: .leading).foregroundStyle(colorHumi).minimumScaleFactor(0.8)
             }.frame(height: rowHeight)
             
             if(weather.alert.count==0){
                 HStack {
-                    MyText("[NEXT]")
+                    MyText("[NEXT]").kerning(0.1)
                     HStack{
                         WXImage(wxIcon: weather.after1Hours.symbol).foregroundStyle(color: colorCond)
                         Text(weather.after1Hours.condition).font(smallFont).frame(alignment: .leading).foregroundStyle(colorCond)
@@ -120,7 +120,7 @@ struct HealthRectangularView : View {
             }.frame(height: rowHeight)
             
             HStack {
-                Text("[KCAL]").font(.system(size: qFontSize-0.5))
+                MyText("[KCAL]", fontSize: qFontSize-0.5)
                 Image(systemName: "flame").imageScale(.small).foregroundStyle(colorKcal)
                 MyText("\(health.excercise)").foregroundStyle(colorKcal)
             }.frame(height: rowHeight)
@@ -140,11 +140,18 @@ struct HealthRectangularView : View {
 
 
 struct MyText: View {
-    let font = Font.system(size: qFontSize)
+    let font: Font
+    
     let text: String
     
     init(_ text: String) {
         self.text = text
+        self.font = Font.custom("SFMono-Light", size: qFontSize)
+    }
+    
+    init(_ text: String, fontSize: CGFloat){
+        self.text = text
+        self.font = Font.custom("SFMono-Light", size: fontSize)
     }
     
     var body: some View{
@@ -157,7 +164,7 @@ struct MyText: View {
     
     VStack(alignment: .leading, spacing: 1) {
               
-        WeatherRectangularView(context: nil, weather: WeatherViewInfo(current: QWeather(date: Date(), condition: "局部小雨", symbol: "cloud.rain", temperature: "20℃",humidity: "50%"), after1Hours: QWeather(date: Date()+3600,condition: "局部大雪", symbol: "snow", temperature: "-11℃",humidity: "50%"),alert: "大风预警"))
+        WeatherRectangularView(context: nil, weather: WeatherViewInfo(current: QWeather(date: Date(), condition: "局部小雨", symbol: "cloud.rain", temperature: "20℃",humidity: "50%"), after1Hours: QWeather(date: Date()+3600,condition: "局部大雪", symbol: "snow", temperature: "-11℃",humidity: "50%"),alert: "", dateText: "周末"))
         
         HealthRectangularView(context: nil, health: HealthInfo(steps: 9999, excercise: 99, excerciseTime: 99, standHours: 99, heartRate: 60))
     }
